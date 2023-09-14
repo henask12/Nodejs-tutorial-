008a83039816dd21db29bc0d2e20c5b0e3a41f96
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-15 02:03:37 +0300

    app js change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const amount = 12;
+//GLOBALS - NO WINDOW!!
 
-if (amount < 10) {
-  console.log("small number");
-} else console.log("large number");
-
-console.log(`hey its my first node app`);
+//  __dirname - path to current directory
+//  __filename - file name
+// require  - function to use modules (CommonJS)
+// module    - info about current module (file)
+// process   - info about env where the program is being executed
