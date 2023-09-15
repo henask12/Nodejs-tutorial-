f10af58b4a7df92fd511cce4379fae781ad90aa2
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-15 15:28:41 +0300

    alternate data

---
+++ 6-alternative.js
@@ -0,0 +1,15 @@
+module.exports.items = [
+  "item",
+  "item1",
+  "item2",
+  "item3",
+  "item4",
+  "item5",
+  "item6",
+];
+const person = {
+  name: "John",
+  age: 30,
+};
+
+module.exports.singlePerson = person;
