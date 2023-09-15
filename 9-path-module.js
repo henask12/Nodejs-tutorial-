85111ae4d73972d1082db5c34607345c95eced38
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-15 18:54:42 +0300

    path module tuts

---
+++ 9-path-module.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+console.log(path.sep);
+
+const filePath = path.join("/content/", "subfolder", "test.txt");
+console.log(filePath);
+
+const base = path.basename(filePath);
+console.log(base);
+
+const absolutePath = path.resolve(
+  __dirname,
+  "content",
+  "subfolder",
+  "test.txt"
+);
+console.log(absolutePath);
