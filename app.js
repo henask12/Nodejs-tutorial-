af0cf1f8d25d33335ee40f09e1909369924d705b
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-15 18:38:30 +0300

    os module update

--- app.js
+++
@@ -1,9 +0,0 @@
-const names = require("./4-names.js");
-const sayHi = require("./5-utils.js");
-const data = require("./6-alternative.js");
-
-require("./7-mind-grenade.js");
-
-sayHi(names.john);
-sayHi(names.peter);
-sayHi("susan");
