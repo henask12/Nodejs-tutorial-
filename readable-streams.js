6f517e5b4ace4aa7902d0e8114d06688b3fcdb25
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-17 21:37:26 +0300

    weriteable stream to the given stream and returns the resulting stream

--- readable-streams.js
+++ readable-streams.js
@@ -6,3 +6,12 @@ const readableStream = fs.createReadStream("example.txt");
 readableStream.on("data", (chunk) => {
   console.log(chunk.toString());
 });
+
+//WritableStream
+const writableStream = fs.createWriteStream("output.txt");
+
+writableStream.write("Hello, world!");
+
+writableStream.on("finish", () => {
+  console.log("The file has been written.");
+});
