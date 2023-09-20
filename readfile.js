c0d32f234f9f4a3e3136f68ae69ccd8cd9f53b09
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-21 01:12:39 +0300

    writefile asyn

--- readfile.js
+++ readfile.js
@@ -7,6 +7,10 @@ const start = async () => {
   try {
     const first = await readFilePromise("./content/first.txt", "utf8");
     const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise(
+      "./content/resultasync.txt",
+      `This is the result: ${first} and ${second}`
+    );
     console.log(first, second);
   } catch (err) {
     console.log(err);
