a39bfa4bd1c5b8fa40bfd28d155d8df1c19e8ead
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-21 01:09:32 +0300

    readFilePromise

--- readfile.js
+++ readfile.js
@@ -1,11 +1,23 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
 const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) reject(err);
+//       resolve(data);
+//     });
+//   });
+// };
